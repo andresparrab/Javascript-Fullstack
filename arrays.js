795268595090941d8fb06d7ca7e4c0f40e738b25
doctor <andres.parrab@gmail.com>
2020-10-10 11:51:32 +0200

    first commit

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var ourArray = ["John",23];
+
+var myArray = ourArray[0];
+console.log(myArray);
+
+var myNestedArray =[["Master",39],["Black box",43]];
+console.log(myNestedArray[0]);
---
+++ arrays02.js
@@ -0,0 +1,8 @@
+var ourArray = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[[13,14,15],16,17,18]];
+var myData = ourArray[4][1];
+console.log(myData);
+
+var myArray = [["andres",23],["cat",34]];
+console.log(myArray);
+myArray.push(["dog",3]);
+console.log(myArray)
