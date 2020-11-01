dfa0ffbe01ceced0f148c02d3f27aa788483d7fb
doctor <andres.parrab@gmail.com>
2020-11-01 12:17:50 +0100

    addnig animated background

--- Projects/my-recipie-app/src/App.js
+++ Projects/my-recipie-app/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Routing from "./routes/Routing";
 import Header from "./components/NavigationBar/Header";
-import "./shared/global/global.css";
+//import "./shared/global/global.css";
+import "./shared/global/backga.css";
 import { UserProvider } from "./shared/global/provider/UserProvider"; // importerar global variabler
 
 function App() {
