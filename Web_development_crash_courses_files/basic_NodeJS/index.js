9630e0c6c5052b17bd3cf85887be2c6e98b66f15
doctor <andres.parrab@gmail.com>
2020-11-08 10:38:09 +0100

    updates

---
+++ Web_development_crash_courses_files/basic_NodeJS/index.js
@@ -0,0 +1,9 @@
+const Person = require('./person')
+
+//console.log( 'Hello Nodejs my name is : ', Person)
+
+const person1 = Person('Andres', 49 )
+
+console.log('this is my name ' ,  person1.name)
+console.log('this is my age ' ,  person1.age)
+person1.greeting()
